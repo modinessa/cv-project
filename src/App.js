d8238cae4d6474b49b8ef2e2ac3272e2c54dc978
Inessa Modina <modina.inessa@gmail.com>
2022-06-01 15:25:24 -0700

    Add TimeLine component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import { Feedback } from './components/Feedback';
 import { NavigationPanel } from './components/NavigationPanel/index';
 
 import { experience } from './components/Expertise/constants';
+import { education } from './components/TimeLine/constants';
 import { users } from './components/Feedback/constants';
 import { aboutMe } from './components/Box/constants';
+
+import { TimeLine } from './components/TimeLine';
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
 			<NavigationPanel />
 			<div className='app-section-container'>
 				<Box title="About me" content={aboutMe}/>
+				<TimeLine data={education} />
 				<Expertise data={experience} />
 				<Feedback users={users} />
 				
--- src/components/TimeLine/index.js
+++ src/components/TimeLine/index.js
@@ -1,1 +1,39 @@
+import './timeLine.scss';
 import React from 'react';
+
+
+export function TimeLine({data}) {
+
+	return (
+		<section className='app-section'>
+			<h2 className='app-title'>
+				Education
+			</h2>
+			<div className='time-line-container'>
+				{data.map((date) => {
+					return (
+						<div className='time-line-item'>
+							<div className='time-line'>
+								<h3>{date.date}</h3>
+								<div className='line-vert'></div>
+							</div>
+							<div className='info-container'>
+								<div className='triangle-left'>
+								</div>
+								<div className='info'>
+									<h3>
+										{date.title}
+									</h3>
+									<p>
+										{date.text}
+									</p>
+								</div>
+							</div>
+						</div>
+					)
+				})}
+			</div>
+			
+		</section>	
+	)
+}
