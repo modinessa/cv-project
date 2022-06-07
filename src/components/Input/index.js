48f1a9607aa0b8db2b5cb7601e40bb3e9972a5b6
Inessa Modina <modina.inessa@gmail.com>
2022-06-07 13:37:27 -0700

    Add validation to edit skills

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,16 +2,29 @@ import './input.scss'
 import React from "react";
 
 export function Input({ register, name, title, errors}) {
+	console.log(errors);
+	let message = "";
+
+	if (errors.hasOwnProperty('message')) {
+		message = errors.message;
+	} else if (name in errors) {
+		message = errors[name].message;
+	}
 
 	return (
-		<div className="data-row">
-				<label htmlFor={name} className="title-column">
-					{`${title}:`}
-				</label>
-				<input
-					className={`data-column skill-input ${errors[name] ? "error" : ""}`}
-					{...register(name)}
-					placeholder={`Enter ${title.toLowerCase()}`}/>
-		</div>
+		<>
+			<div className="data-row">
+					<label htmlFor={name} className="title-column">
+						{`${title}:`}
+					</label>
+					<input
+						className={`data-column skill-input ${errors[name] ? "error" : ""}`}
+						{...register(name)}
+						placeholder={`Enter ${title.toLowerCase()}`}/>
+			</div>
+			<div className='error-message'>
+				{message}
+			</div>
+		</>
 	)
 }
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -4,23 +4,28 @@ import React, { useCallback } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
 import { setEditSkillsIsHide, setSkills } from '../../reducers';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import * as yup from 'yup';
 
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import { Button } from '../Button';
 import { Input } from '../Input';
 import { RangeBar } from './RangeBar';
 import { Ruler } from './Ruler';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
 
 import { updateSkills } from './utils/updateSkills';
 
 const schema = yup.object({
-  skill: yup.string().required(),
-  range: yup.number().positive().min(10).max(100).required(),
-}).required();
+  skill: yup.string('Enter skill')
+			.required('Skill name is a required field'),
+  range: yup.number('Skill range must be a number from 10 to 100')
+			.typeError('Skill range must be a number from 10 to 100')
+			.required('Skill range is a required field')
+			.min(10, 'Skill range must be greater or equal to 10')
+			.max(100, 'Skill range must be less or equal to 10'),
+});
 
 export function Skills() {
 
@@ -28,10 +33,13 @@ export function Skills() {
 	const dispatch = useDispatch();
 
 	const {
+		reset,
 		register,
 		handleSubmit,
-		formState:{ errors }
+		formState:{ errors, isDirty }
 	} = useForm({resolver: yupResolver(schema)});
+
+	const isError = Object.keys(errors).length > 0;
 
 	const clickHandler = useCallback(() => {
 		dispatch(setEditSkillsIsHide(!editSkillsIsHide));
@@ -40,39 +48,40 @@ export function Skills() {
 	const onSubmit = useCallback((data) =>  {
 		let updatedSkills = updateSkills(data, skills);
 		dispatch(setSkills(updatedSkills));
-		localStorage.setItem("Skills", JSON.stringify(updatedSkills));
+		localStorage.setItem('Skills', JSON.stringify(updatedSkills));
+		reset()
 	}, [skills, dispatch]);
 
 	return (
-		<section id="skills" className='app-section'>
+		<section id='skills' className='app-section'>
 			<div className='skills-title'>
 				<h2 className='app-title'>
 					Skills
 				</h2>
 				<Button
 							icon={<FontAwesomeIcon icon={faPencil}/>}
-							text={editSkillsIsHide ? "Open edit" : "Close edit"}
+							text={editSkillsIsHide ? 'Open edit' : 'Close edit'}
 							clickHandler={clickHandler}
 				/>
 			</div>
 			{!editSkillsIsHide && 
 				<div className='setSkills '>
 					<form className='edit-skills' onSubmit={handleSubmit(onSubmit)}>
-						<Input name="skill"
-									title="Skill name"
+						<Input name='skill'
+									title='Skill name'
 									register={register}
 									errors={errors}
 						/>
-						<Input name="range"
-									title="Skill range"
+						<Input name='range'
+									title='Skill range'
 									register={register}
 									errors={errors}
 						/>
 						<input 
-									type="submit"
+									type='submit'
 									className='submit-btn'
-									value="Add skill"
-									disabled={!errors}/>
+									value='Add skill'
+									disabled={!isDirty || isError}/>
 					</form>
 				</div>
 			}
